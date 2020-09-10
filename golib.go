9989b8d0a56b0a9514b7c68f313e6ce4aa8e31c9
Jenny Roan <jennyroan@bigobject.io>
2020-09-10 15:50:32 +0800

    remove space & cctv

--- golib.go
+++ golib.go
@@ -5,8 +5,6 @@ import (
 	"github.com/bigobject-inc/golib/irisweb"
 	"github.com/bigobject-inc/golib/logger"
 	"github.com/bigobject-inc/golib/miscellaneous"
-	"github.com/bigobject-inc/golib/space"
-	"github.com/bigobject-inc/golib/space/cctv"
 )
 
 // GoLibrary golang library service
@@ -17,8 +15,6 @@ type GoLibrary interface {
 	NewRotationLogger(loggerFile, loggerLevel string) (logger.Logger, error)
 	NewWeb(l logger.Logger, conf irisweb.Configure, rootPath string) (irisweb.Web, error)
 	NewWebController(path, method, name, summary string) (irisweb.Controller, error)
-	GetServGeometry() space.Geometry
-	NewCCTV(id string, geolocation [2]float64, resolution []interface{}, cameraMatrix [][]float64, rmat [][]float64, tvec [][]float64, dist [][]float64) cctv.CCTV
 	NewPostgres(username, password, ip, port, database string) dbquery.Dbquery
 	NewBO(username, password, ip, port, database string) dbquery.Dbquery
 	NewBOQuery(username, password, ip, port, database string) dbquery.Boquery
